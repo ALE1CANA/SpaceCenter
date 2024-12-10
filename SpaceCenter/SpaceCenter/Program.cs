ce031d433d4c11713e3dfa5c40cbed081476ccfb
ALE1CANA <alessandro.salvi07@icloud.com>
2024-12-10 22:15:59 +0100

    Add files via upload

---
+++ SpaceCenter/SpaceCenter/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceCenter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread houstonThread = new Thread(HoustonCountdown);
+            Thread capeCanaveralThread = new Thread(CapeCanaveralCountdown);
+
+            houstonThread.Start();
+            capeCanaveralThread.Start();
+
+            houstonThread.Join();
+            capeCanaveralThread.Join();
+
+            Console.WriteLine("Rampa di lancio: Si parte!!!");
+            Console.ReadLine();
+        }
+
+        static void HoustonCountdown()
+        {
+            Countdown("Houston");
+        }
+
+        static void CapeCanaveralCountdown()
+        {
+            Countdown("Cape Canaveral");
+        }
+
+        static void Countdown(string stazione)
+        {
+            for (int i = 10; i > 0; i--)
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine(stazione + ": "+ i);
+            }
+            Thread.Sleep(1000);
+            Console.WriteLine(stazione + ": Go!");
+        }
+    }
+
+}
